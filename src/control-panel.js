b9933e72b678c2fcd51d03ed5effa127d37dc371
Amory Galili <amorygalili@gmail.com>
2020-02-17 19:40:14 -0500

    You can now set the current color of the wheel, which animates it to the correct position

--- src/control-panel.js
+++ src/control-panel.js
@@ -13,19 +13,37 @@ class ControlPanel extends Webbit {
 
       svg {
         height: 100%;
+        transition: all 2s;
       }
     `;
   }
 
   static get properties() {
     return {
-      
+      desiredColor: {type: String, attribute:'desired-color'},
+      currentColor: {type: String, attribute:'current-color'},
+      desiredRotations: {type: Number, attribute: 'desired-rotations'},
+      mode: {type: String, attribute:'mode'},
+      currentRotations: {type: Number, attribute: 'current-rotations'},
+      wheelPosition: { type: Number, attribute: false }
     };
   }
 
   constructor() {
     super();
     this.colors = ['red','yellow','blue','green','red','yellow','blue','green'];
+    this.desiredColor = 'unknown';
+    this.currentColor = 'unknown';
+    this.desiredRotations = 0;
+    this.currentRotations = 0;
+    this.mode = 'disable';
+    this.wheelPosition = 0;
+    this.startingWheelPosition = {
+      'red': 157.5, 
+      'green': 22.5, 
+      'blue': 67.5, 
+      'yellow': 112.5
+    };
 
   }
 
@@ -45,11 +63,35 @@ class ControlPanel extends Webbit {
     ].join(' ');
   }
 
+  updated(changedProperties) {
+    console.log('changedProperties:', changedProperties);
+
+    console.log('this.currentColor:', this.currentColor);
+
+
+    if (changedProperties.has('currentColor')) {
+      if(changedProperties.get('currentColor') === 'unknown') {
+        this.wheelPosition = this.startingWheelPosition[this.currentColor];
+      } else if (this.currentColor !== 'unknown') {
+        const colors = ['red','yellow','blue','green'];
+        const prevColorIndex = colors.indexOf(changedProperties.get('currentColor'));
+        const colorIndex = colors.indexOf(this.currentColor);
+        let positionDelta = (colorIndex - prevColorIndex);
+        if (positionDelta < 0) {
+          positionDelta += 4;
+        }
+        this.wheelPosition += positionDelta * -45.0;
+      }
+    }
+  
+  }
+
   render() {
     return svg`
-      <svg viewBox="-1 -1 2 2" style="transform: rotate(-90deg)">
+      <svg viewBox="-1 -1 2 2" style="transform: rotate(${this.wheelPosition}deg)">
         ${this.colors.map((color, index) => svg`
-          <path 
+          <path
+            @click="${() => console.log(color)}"
             fill="${color}" 
             d="${this.getPath(index)}"
           ></path> 
